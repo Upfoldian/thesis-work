d3cc65fea87f8750380cc31ef02d2cae7e021a6d
Thomas Upfold <thomasupfold@gmail.com>
2018-10-16 01:38:17 +1100

    Testing GPIO pins on rpi0w with python

---
+++ rpi/GPIO tests/twoButtonLED.py
@@ -0,0 +1,12 @@
+from gpiozero import LED
+from time import sleep
+
+ledR = LED(17)
+ledL = LED(27)
+while True:
+    ledL.on()
+    ledR.off()
+    sleep(1)
+    ledL.off()
+    ledR.on()
+    sleep(1)
