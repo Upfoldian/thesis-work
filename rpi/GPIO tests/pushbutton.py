b24de478db920f5f4d2bdd753a2d5e89dfa05351
Thomas Upfold <thomasupfold@gmail.com>
2018-10-25 17:46:45 +1100

    added pushbutton test

---
+++ rpi/GPIO tests/pushbutton.py
@@ -0,0 +1,12 @@
+import gpiozero
+from time import sleep
+
+led= LED(17)
+button = Button(27)
+#ledL = LED(27)
+while True:
+	if (button.is_pressed):
+		sleep(0.1)
+		led.on()
+    else:
+    	led.off()
